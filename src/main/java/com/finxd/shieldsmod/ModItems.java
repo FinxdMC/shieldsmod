3fb8804d9e6f5ba14e46a119fa5e34d6a08ba307
FinxdMC <Timphaapsaari@gmail.com>
2023-05-15 18:15:09 +0300

    mod version & shield durability changes

--- src/main/java/com/finxd/shieldsmod/ModItems.java
+++ src/main/java/com/finxd/shieldsmod/ModItems.java
@@ -15,10 +15,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> WOODEN_SHIELD = ITEMS.register("wooden_shield", () -> new WoodenShieldItem((new Item.Properties()).durability(55)));
     public static final RegistryObject<Item> GOLDEN_SHIELD = ITEMS.register("golden_shield", () -> new GoldenShieldItem((new Item.Properties()).durability(77)));
-    public static final RegistryObject<Item> COPPER_SHIELD = ITEMS.register("copper_shield", () -> new CopperShieldItem((new Item.Properties()).durability(115)));
-    public static final RegistryObject<Item> IRON_SHIELD = ITEMS.register("iron_shield", () -> new IronShieldItem((new Item.Properties()).durability(165)));
-    public static final RegistryObject<Item> DIAMOND_SHIELD = ITEMS.register("diamond_shield", () -> new DiamondShieldItem((new Item.Properties()).durability(363)));
-    public static final RegistryObject<Item> NETHERITE_SHIELD = ITEMS.register("netherite_shield", () -> new NetheriteShieldItem((new Item.Properties()).durability(407).fireResistant()));
+    public static final RegistryObject<Item> COPPER_SHIELD = ITEMS.register("copper_shield", () -> new CopperShieldItem((new Item.Properties()).durability(125)));
+    public static final RegistryObject<Item> IRON_SHIELD = ITEMS.register("iron_shield", () -> new IronShieldItem((new Item.Properties()).durability(337)));
+    public static final RegistryObject<Item> DIAMOND_SHIELD = ITEMS.register("diamond_shield", () -> new DiamondShieldItem((new Item.Properties()).durability(563)));
+    public static final RegistryObject<Item> NETHERITE_SHIELD = ITEMS.register("netherite_shield", () -> new NetheriteShieldItem((new Item.Properties()).durability(637).fireResistant()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
