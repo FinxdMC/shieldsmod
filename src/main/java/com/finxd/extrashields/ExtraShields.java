7c9b9dfbbd4c22e58018d1efa84916fb5c42f42e
FinxdMC <Timphaapsaari@gmail.com>
2023-06-08 17:26:01 +0300

    initial commit

---
+++ src/main/java/com/finxd/extrashields/ExtraShields.java
@@ -0,0 +1,51 @@
+package com.finxd.extrashields;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExtraShields.MODID)
+public class ExtraShields {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "extrashields";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExtraShields() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        ModItems.addCustomItemProperties();
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.WOODEN_SHIELD);
+            event.accept(ModItems.GOLDEN_SHIELD);
+            event.accept(ModItems.COPPER_SHIELD);
+            event.accept(ModItems.IRON_SHIELD);
+            event.accept(ModItems.DIAMOND_SHIELD);
+            event.accept(ModItems.NETHERITE_SHIELD);
+        }
+    }
+}
